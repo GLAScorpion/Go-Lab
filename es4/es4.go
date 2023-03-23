876a4850f0c6ddcda070155d45440d2fdcc1340a
GLAScorpion <pier.colella122@gmail.com>
2023-03-23 14:58:01 +0100

    Update es4.go

--- es4/es4.go
+++ es4/es4.go
@@ -20,9 +20,9 @@ var (
 )
 
 var (
-	timeEUR = time.Time{}
-	timeGBP = time.Time{}
-	timeJPY = time.Time{}
+	sellTimeEUR = time.Time{}
+	buyTimeGBP  = time.Time{}
+	buyTimeJPY  = time.Time{}
 )
 
 var (
@@ -39,13 +39,13 @@ func main() {
 
 func simulateMarketData() {
 	for {
-		if !sellEUR || (sellEUR && time.Since(timeEUR).Seconds() >= 4.0) {
+		if !sellEUR || (sellEUR && time.Since(sellTimeEUR).Seconds() >= 4.0) {
 			eur_usd <- float64(int32(rand.Int31()))/float64(math.MaxInt32)*0.5 + 1.0
 		}
-		if !buyGBP || (buyGBP && time.Since(timeGBP).Seconds() >= 3.0) {
+		if !buyGBP || (buyGBP && time.Since(buyTimeGBP).Seconds() >= 3.0) {
 			gbp_usd <- float64(int32(rand.Int31()))/float64(math.MaxInt32)*0.5 + 1.0
 		}
-		if !buyJPY || (buyJPY && time.Since(timeJPY).Seconds() >= 3.0) {
+		if !buyJPY || (buyJPY && time.Since(buyTimeJPY).Seconds() >= 3.0) {
 			jpy_usd <- float64(int32(rand.Int31()))/float64(math.MaxInt32)*0.003 + 0.006
 		}
 		time.Sleep(time.Second)
@@ -59,29 +59,29 @@ func selectPair() {
 		case val := <-eur_usd:
 			if val > 1.20 && !sellEUR {
 				holdEUR = val
-				timeEUR = time.Now()
+				sellTimeEUR = time.Now()
 				sellEUR = true
 			} else if sellEUR {
 				sellEUR = false
-				fmt.Println("Detected EUR/USD at", holdEUR, ", sold at", val, time.Since(timeEUR))
+				fmt.Println("Detected EUR/USD at", holdEUR, ", sold at", val, time.Since(sellTimeEUR))
 			}
 		case val := <-gbp_usd:
 			if val < 1.35 && holdGBP == 0 {
 				holdGBP = val
-				timeGBP = time.Now()
+				buyTimeGBP = time.Now()
 				buyGBP = true
 			} else if buyGBP {
 				buyGBP = false
-				fmt.Println("Detected GBP/USD at", holdGBP, ", bought at", val, time.Since(timeGBP))
+				fmt.Println("Detected GBP/USD at", holdGBP, ", bought at", val, time.Since(buyTimeGBP))
 			}
 		case val := <-jpy_usd:
 			if val < 0.0085 && holdJPY == 0 {
 				holdJPY = val
-				timeJPY = time.Now()
+				buyTimeJPY = time.Now()
 				buyJPY = true
 			} else if buyJPY {
 				buyJPY = false
-				fmt.Println("Detected JPY/USD at", holdJPY, ", bought at", val, time.Since(timeJPY))
+				fmt.Println("Detected JPY/USD at", holdJPY, ", bought at", val, time.Since(buyTimeJPY))
 			}
 		}
 		//time.Sleep(500 * time.Millisecond)
