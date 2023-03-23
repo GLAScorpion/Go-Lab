9671a5c5466ff52824d5cfeebd1286502dd0b5d9
GLAScorpion <pier.colella122@gmail.com>
2023-03-23 15:27:45 +0100

    Update es4.go

--- es4/es4.go
+++ es4/es4.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -23,12 +24,21 @@ var (
 	sellTimeEUR = time.Time{}
 	buyTimeGBP  = time.Time{}
 	buyTimeJPY  = time.Time{}
+	timeEUR     = time.Time{}
+	timeGBP     = time.Time{}
+	timeJPY     = time.Time{}
 )
 
 var (
 	sellEUR = false
 	buyGBP  = false
 	buyJPY  = false
+)
+
+var (
+	mutexEUR = sync.RWMutex{}
+	mutexGBP = sync.RWMutex{}
+	mutexJPY = sync.RWMutex{}
 )
 
 func main() {
@@ -38,17 +48,35 @@ func main() {
 }
 
 func simulateMarketData() {
+	timeEUR = time.Now()
+	timeGBP = time.Now()
+	timeJPY = time.Now()
 	for {
-		if !sellEUR || (sellEUR && time.Since(sellTimeEUR).Seconds() >= 4.0) {
+		mutexEUR.Lock()
+		if (!sellEUR && time.Since(timeEUR).Seconds() >= 1.0) || (sellEUR && time.Since(sellTimeEUR).Seconds() >= 4.0) {
+			timeEUR = time.Now()
+			mutexEUR.Unlock()
 			eur_usd <- float64(int32(rand.Int31()))/float64(math.MaxInt32)*0.5 + 1.0
+		} else {
+			mutexEUR.Unlock()
 		}
-		if !buyGBP || (buyGBP && time.Since(buyTimeGBP).Seconds() >= 3.0) {
+		mutexGBP.Lock()
+		if (!buyGBP && time.Since(timeGBP).Seconds() >= 1.0) || (buyGBP && time.Since(buyTimeGBP).Seconds() >= 3.0) {
+			timeGBP = time.Now()
+			mutexGBP.Unlock()
 			gbp_usd <- float64(int32(rand.Int31()))/float64(math.MaxInt32)*0.5 + 1.0
+		} else {
+			mutexGBP.Unlock()
 		}
-		if !buyJPY || (buyJPY && time.Since(buyTimeJPY).Seconds() >= 3.0) {
+		mutexJPY.Lock()
+		if (!buyJPY && time.Since(timeJPY).Seconds() >= 1.0) || (buyJPY && time.Since(buyTimeJPY).Seconds() >= 3.0) {
+			timeJPY = time.Now()
+			mutexJPY.Unlock()
 			jpy_usd <- float64(int32(rand.Int31()))/float64(math.MaxInt32)*0.003 + 0.006
+		} else {
+			mutexJPY.Unlock()
 		}
-		time.Sleep(time.Second)
+		//time.Sleep(time.Second)
 	}
 }
 
@@ -57,6 +85,7 @@ func selectPair() {
 	for {
 		select {
 		case val := <-eur_usd:
+			mutexEUR.Lock()
 			if val > 1.20 && !sellEUR {
 				holdEUR = val
 				sellTimeEUR = time.Now()
@@ -65,8 +94,10 @@ func selectPair() {
 				sellEUR = false
 				fmt.Println("Detected EUR/USD at", holdEUR, ", sold at", val, time.Since(sellTimeEUR))
 			}
+			mutexEUR.Unlock()
 		case val := <-gbp_usd:
-			if val < 1.35 && holdGBP == 0 {
+			mutexGBP.Lock()
+			if val < 1.35 && !buyGBP {
 				holdGBP = val
 				buyTimeGBP = time.Now()
 				buyGBP = true
@@ -74,8 +105,10 @@ func selectPair() {
 				buyGBP = false
 				fmt.Println("Detected GBP/USD at", holdGBP, ", bought at", val, time.Since(buyTimeGBP))
 			}
+			mutexGBP.Unlock()
 		case val := <-jpy_usd:
-			if val < 0.0085 && holdJPY == 0 {
+			mutexJPY.Lock()
+			if val < 0.0085 && buyJPY {
 				holdJPY = val
 				buyTimeJPY = time.Now()
 				buyJPY = true
@@ -83,6 +116,7 @@ func selectPair() {
 				buyJPY = false
 				fmt.Println("Detected JPY/USD at", holdJPY, ", bought at", val, time.Since(buyTimeJPY))
 			}
+			mutexJPY.Unlock()
 		}
 		//time.Sleep(500 * time.Millisecond)
 	}
