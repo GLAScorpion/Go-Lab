d64ad72850e815756b95a65e0de8e15acdb56680
GLAScorpion <pier.colella122@gmail.com>
2023-07-07 15:48:19 +0200

    Aggiornamenti commenti

--- es3/es3.go
+++ es3/es3.go
@@ -1,5 +1,9 @@
 package main
 
+/*
+	Tramite l'utilizzo di mappe e mutex è possibile tenere traccia delle torte lavorate
+	per poter fornire una stampa a video dello stadio in cui si trovano
+*/
 import (
 	"fmt"
 	"sync"
@@ -26,7 +30,7 @@ var bakedCakes = make(map[int]cake)     // torte cotte
 var garnishedCakes = make(map[int]cake) // torte guarnite
 var doneCakes = make(map[int]cake)      // torte complete
 
-var wg = sync.WaitGroup{}
+var wg = sync.WaitGroup{} // waitgroup dei pasticceri
 
 var uncookMutex = sync.RWMutex{}
 var bakeMutex = sync.RWMutex{}
@@ -52,7 +56,7 @@ func main() {
 	}()
 	wg.Wait()
 	printer()
-	elapsed := int(time.Since(startTime).Seconds()) // tempo trascorso per produrre le torte
+	elapsed := float64(time.Since(startTime).Seconds()) // tempo trascorso per produrre le torte
 	fmt.Println("It took", elapsed, "seconds")
 }
 
@@ -84,68 +88,68 @@ func bake(baker *worker) {
 	bakeMutex.Lock()
 	baker.willWork = false //non produrrà altre torte
 	bakeMutex.Unlock()
-	wg.Done()
+	wg.Done() // finito di lavorare
 }
 
 func garnish(garnisher *worker, baker *worker) {
 	for {
-		seeker := 0
+		seeker := 0 //indice per scorrere gli spazi dove mettere le torte complete
 	garnisherLoop:
 		for {
 			garnishMutex.Lock()
-			if _, ok := garnishedCakes[seeker]; !ok {
+			if _, ok := garnishedCakes[seeker]; !ok { // lavora solo se c'è uno spazio libero tra i 2 slot a disposizione
 				garnishMutex.Unlock()
-				seeker2 := 0
+				seeker2 := 0 //indice per scorrere gli spazi dove si trovano torte da lavorare
 				for {
 					bakeMutex.Lock()
-					if val, ok := bakedCakes[seeker2]; ok {
-						delete(bakedCakes, seeker2)
+					if val, ok := bakedCakes[seeker2]; ok { // cerca una torta tra quelle da lavorare
+						delete(bakedCakes, seeker2) // preleva uan torta da lavorare
 						bakeMutex.Unlock()
-						time.Sleep(kGarnishTime * time.Millisecond)
+						time.Sleep(kGarnishTime * time.Millisecond) // lavoro
 						garnishMutex.Lock()
-						garnishedCakes[seeker] = val
+						garnishedCakes[seeker] = val //posiziona torta nello slot vuoto
 						garnishMutex.Unlock()
 						break garnisherLoop
-					} else if !baker.willWork && seeker2 == 1 {
+					} else if !baker.willWork && seeker2 == 1 { // se il pasticcere precedente non lavorererà più e non ci sono più torte da lavorare termina
 						bakeMutex.Unlock()
-						garnisher.willWork = false
-						wg.Done()
+						garnisher.willWork = false //non produrrà altre torte
+						wg.Done()                  // finito di lavorare
 						return
 					} else {
 						bakeMutex.Unlock()
 					}
-					seeker2 = (seeker2 + 1) % 2
+					seeker2 = (seeker2 + 1) % 2 // gli unici indici validi sono 2: 0 e 1
 				}
 			} else {
 				garnishMutex.Unlock()
 			}
-			seeker = (seeker + 1) % 2
+			seeker = (seeker + 1) % 2 // gli unici indici validi sono 2: 0 e 1
 		}
 	}
 }
 
 func decorate(garnisher *worker) {
 	for i := 0; ; i++ {
-		seeker := 0
+		seeker := 0 //indice per scorrere gli spazi dove si trovano torte da lavorare
 		for {
 			garnishMutex.Lock()
-			if val, ok := garnishedCakes[seeker]; ok {
+			if val, ok := garnishedCakes[seeker]; ok { // cerca una torta tra quelle da lavorare
 				delete(garnishedCakes, seeker)
 				garnishMutex.Unlock()
-				time.Sleep(kDecorateTime * time.Millisecond)
+				time.Sleep(kDecorateTime * time.Millisecond) // lavoro
 				doneMutex.Lock()
 				doneCakes[i] = val
 				doneMutex.Unlock()
 				break
-			} else if !garnisher.willWork && seeker == 1 {
+			} else if !garnisher.willWork && seeker == 1 { // se il pasticcere precedente non lavorererà più e non ci sono più torte da lavorare termina
 				garnishMutex.Unlock()
-				wg.Done()
+				wg.Done() // finito di lavorare
 				return
 			} else {
 				garnishMutex.Unlock()
 			}
 
-			seeker = (seeker + 1) % 2
+			seeker = (seeker + 1) % 2 // gli unici indici validi sono 2: 0 e 1
 		}
 	}
 }
--- es4/es4.go
+++ es4/es4.go
@@ -1,5 +1,10 @@
 package main
 
+/*
+	Ho inteso le transazioni come possibili in simultanea per tipi differenti di valuta.
+	Prima di poter effettuare una transazione con la stessa valuta bisognerà aspettare il
+	tempo di necessario a completare l'operazione precedente, se in corso
+*/
 import (
 	"fmt"
 	"math"
@@ -7,23 +12,25 @@ import (
 	"time"
 )
 
-var eurUSD = make(chan float64, 5)
-var gbpUSD = make(chan float64, 5)
-var jpyUSD = make(chan float64, 5)
-
-var (
+var ( // canali delle valute
+	eurUSD = make(chan float64)
+	gbpUSD = make(chan float64)
+	jpyUSD = make(chan float64)
+)
+var ( // salvano il valore di rilevazione delle valute
 	holdEUR = 0.0
 	holdGBP = 0.0
 	holdJPY = 0.0
 )
 
-var (
-	timeEUR = time.Time{}
-	timeGBP = time.Time{}
-	timeJPY = time.Time{}
+var ( // tempo di acquisto delle valute
+	timeEUR     = time.Time{}
+	timeGBP     = time.Time{}
+	timeJPY     = time.Time{}
+	timeTracker = time.Time{}
 )
 
-var (
+var ( // stato delle transazioni (true = in corso, false = non in corso)
 	sellEUR = false
 	buyGBP  = false
 	buyJPY  = false
@@ -32,10 +39,11 @@ var (
 func main() {
 	go simulateMarketData()
 	go selectPair()
+	timeTracker = time.Now() // tracking del tempo di esecuzione
 	time.Sleep(time.Minute)
 }
 
-func simulateMarketData() {
+func simulateMarketData() { // genera valori randomici ogni secondo
 	for {
 		eurUSD <- float64(int32(rand.Int31()))/float64(math.MaxInt32)*0.5 + 1.0
 		gbpUSD <- float64(int32(rand.Int31()))/float64(math.MaxInt32)*0.5 + 1.0
@@ -44,37 +52,45 @@ func simulateMarketData() {
 	}
 }
 
+func elapsedTimePrinter() {
+	fmt.Print("[", int(time.Since(timeTracker).Seconds()), " seconds passed] ")
+}
+
 func selectPair() {
+	// variabili di tempo azzerate
 	timeEUR = time.Now()
 	timeGBP = time.Now()
 	timeJPY = time.Now()
 	for {
 		select {
 		case val := <-eurUSD:
-			if val > 1.20 && !sellEUR {
-				holdEUR = val
-				timeEUR = time.Now()
-				sellEUR = true
-			} else if sellEUR && time.Since(timeEUR).Seconds() >= 4.0 {
-				sellEUR = false
+			if val > 1.20 && !sellEUR { // inizia operazione se il valore è accettabile e non ci sono operazioni simili già in corso
+				holdEUR = val        // salvo valore a inizio operazione
+				timeEUR = time.Now() // azzero il tempo
+				sellEUR = true       // operazione in corso
+			} else if sellEUR && time.Since(timeEUR).Seconds() >= 4.0 { // completa operazione solo se in corso e dopo un tempo accettabile
+				sellEUR = false // termine operazione
+				elapsedTimePrinter()
 				fmt.Println("Detected EUR/USD at", holdEUR, ", sold at", val, ". It took", time.Since(timeEUR).Seconds(), "seconds")
 			}
 		case val := <-gbpUSD:
-			if val < 1.35 && !buyGBP {
-				holdGBP = val
-				timeGBP = time.Now()
-				buyGBP = true
-			} else if buyGBP && time.Since(timeGBP).Seconds() >= 3.0 {
-				buyGBP = false
+			if val < 1.35 && !buyGBP { // inizia operazione se il valore è accettabile e non ci sono operazioni simili già in corso
+				holdGBP = val        // salvo valore a inizio operazione
+				timeGBP = time.Now() // azzero il tempo
+				buyGBP = true        // operazione in corso
+			} else if buyGBP && time.Since(timeGBP).Seconds() >= 3.0 { // completa operazione solo se in corso e dopo un tempo accettabile
+				buyGBP = false // termine operazione
+				elapsedTimePrinter()
 				fmt.Println("Detected GBP/USD at", holdGBP, ", bought at", val, ". It took", time.Since(timeGBP).Seconds(), "seconds")
 			}
 		case val := <-jpyUSD:
-			if val < 0.0085 && !buyJPY {
-				holdJPY = val
-				timeJPY = time.Now()
-				buyJPY = true
-			} else if buyJPY && time.Since(timeJPY).Seconds() >= 3.0 {
-				buyJPY = false
+			if val < 0.0085 && !buyJPY { // inizia operazione se il valore è accettabile e non ci sono operazioni simili già in corso
+				holdJPY = val        // salvo valore a inizio operazione
+				timeJPY = time.Now() // azzero il tempo
+				buyJPY = true        // operazione in corso
+			} else if buyJPY && time.Since(timeJPY).Seconds() >= 3.0 { // completa operazione solo se in corso e dopo un tempo accettabile
+				buyJPY = false // termine operazione
+				elapsedTimePrinter()
 				fmt.Println("Detected JPY/USD at", holdJPY, ", bought at", val, ". It took", time.Since(timeJPY).Seconds(), "seconds")
 			}
 		}
